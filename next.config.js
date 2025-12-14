1d23fa892a7f31795c6ad5ab2f341b1ccdb73731
Jason Zeng <jasonzeng124@gmail.com>
2025-12-14 07:27:49 -0500

    fix links?

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({ pageExtensions: ['js', 'mdx'] })
+
+// Configure Next for GitHub Pages project site under /hillshacks2026/
+module.exports = withMDX({
+	pageExtensions: ['js', 'mdx'],
+	// basePath makes all internal routes served under this prefix
+	basePath: '/hillshacks2026',
+	// assetPrefix ensures static assets (images, css) are served from the prefix
+	assetPrefix: '/hillshacks2026/',
+	// export as static files with trailing slashes to work well on GitHub Pages
+	trailingSlash: true,
+})
